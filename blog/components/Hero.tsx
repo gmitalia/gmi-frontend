07c2d05db89b117f1f020a4ea78e863b272e6746
Davide Scarioni <davide.scarioni@gmail.com>
2023-01-30 19:21:39 +0100

    Fix some paddings | Prettify some files

--- blog/components/Hero.tsx
+++ blog/components/Hero.tsx
@@ -1,21 +1,31 @@
 import GMILogo from "./GMILogo";
-import HeroImage from "../public/images/HompageHero.png"
+import HeroImage from "../public/images/HompageHero.png";
 
-console.log(HeroImage.src)
+console.log(HeroImage.src);
 
 const Hero = () => {
   return (
-    <div className='bg-gray-800 text-gray-100 border-y border-gray-100 py-10' style={{backgroundImage: `url(${HeroImage.src})`
-  }}>
+    <div
+      className="bg-gray-800 text-gray-100 border-y border-gray-100 py-10"
+      style={{ backgroundImage: `url(${HeroImage.src})` }}
+    >
       <div className="max-w-7xl mx-auto flex justify-between items-center">
-        <div className='space-y-5 max-w-7xl'>
-          <h1 className='text-6xl max-w-xl font-serif'><span className='underline decoration-gray-100  text-gray-100 decoration-4'>GMI</span> &egrave; la community italiana di GameMaker</h1>
-          <h2>Un software che permette di creare videogiochi in modo veloce e divertente.</h2>
+        <div className="space-y-5 p-5 max-w-7xl">
+          <h1 className="text-6xl max-w-xl font-serif">
+            <span className="underline decoration-gray-100  text-gray-100 decoration-4">
+              GMI
+            </span>{" "}
+            &egrave; la community italiana di GameMaker
+          </h1>
+          <h2>
+            Un software che permette di creare videogiochi in modo veloce e
+            divertente.
+          </h2>
         </div>
-        <GMILogo className="fill-white max-w-sm" />
+        <GMILogo className="fill-white max-w-sm p-5" />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Hero;
--- blog/pages/index.tsx
+++ blog/pages/index.tsx
@@ -22,7 +22,7 @@ export default function Home({posts}: Props) {
 
     <Hero />
 
-    <div className="max-w-7xl mx-auto py-3">
+    <div className="max-w-7xl mx-auto p-5">
       <h2 className="text-3xl mb-3">Blog</h2>
       {/* posts */}
       <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 py-3'>
