ace404f30420cf3614836c9eb5a8fe75ffac7045
Davide Scarioni <davide.scarioni@gmail.com>
2023-02-06 17:27:16 +0100

    Some update to mobile header menu - Need a button to collapse it on mobile

--- blog/components/Header/Header.tsx
+++ blog/components/Header/Header.tsx
@@ -5,23 +5,20 @@ import Navbar from "./Navbar";
 const Header = () => {
   const [openMenu, setOpenMenu] = useState(false);
   return (
-    <header className="flex justify-between p-5 max-w-7xl mx-auto">
-      <div className="flex space-x-5 items-center w-full gap-5">
-        <Link href={"/"}>
-          <img
-            className="w-20 object-contain cursor-pointer"
-            src="/images/gmi_logo.png"
-            alt=""
-          />
-        </Link>
-        <Navbar openMenu={openMenu} setOpenMenu={setOpenMenu} />
-        <div className="items-center justify-end">
-          <div className="flex justify-center text-white bg-gray-800 px-4 py-1 rounded-full">
-            <Link href="https://discord.gg/0wKBBPIbX2r3S32a">
-              Entra su Discord
-            </Link>
-          </div>
+    <header className="md:flex md:justify-between p-5 max-w-7xl mx-auto bg-white">
+      <div className="block md:flex md:space-x-5 items-center w-full gap-5">
+        <div className="flex items-center space-x-5">
+          <Link href={"/"}>
+            <img
+              className="w-20 object-contain cursor-pointer"
+              src="/images/gmi_logo.png"
+              alt=""
+            />
+          </Link>
+          <span className="text-3xl hidden lg:block ">GameMaker Italia</span>
         </div>
+        <Navbar openMenu={openMenu} setOpenMenu={setOpenMenu} />
+
         <button className="hidden" onClick={() => setOpenMenu(!openMenu)}>
           Menu
         </button>
--- blog/components/Header/Navbar.tsx
+++ blog/components/Header/Navbar.tsx
@@ -9,21 +9,30 @@ type NavbarProps = {
 const Navbar = ({ openMenu, setOpenMenu }: NavbarProps) => {
   return (
     <ul
-      className={`text-lg hidden md:inline-flex items-center justify-end space-x-5 flex-1`}
+      className={`ml-0 text-xl flex flex-col items-start md:flex-row md:justify-end md:items-center space-x-5 flex-1 z-1 md:z-auto absolute md:static w-full bg-white left-0`}
     >
-      <li>
+      <li className="border-transparent hover:underline decoration-2 duration-500 mx-4 my-6 md:my-0">
         <Link href="/competizioni">Competizioni</Link>
       </li>
-      <li>
+      <li className="border-transparent hover:underline decoration-2  duration-500 mx-4 my-6 md:my-0">
         <Link href="https://gmitalia.altervista.org/forum">Forum</Link>
       </li>
-      <li>
+      <li className="border-transparent hover:underline decoration-2  duration-500 mx-4 my-6 md:my-0">
         <Link href="https://gmiscores.altervista.org">Classifiche</Link>
       </li>
-      <li>
+      <li className="border-transparent hover:underline decoration-2  duration-500 mx-4 my-6 md:my-0">
         <Link href="https://gmitalia.altervista.org/site/risorse-2/">
           Risorse
         </Link>
+      </li>
+      <li className=" mx-4 my-6 md:my-0">
+        <div className="items-center justify-end">
+          <div className="flex justify-center text-white bg-gray-800 px-4 py-1 rounded-full">
+            <Link href="https://discord.gg/0wKBBPIbX2r3S32a">
+              Entra su Discord
+            </Link>
+          </div>
+        </div>
       </li>
     </ul>
   );
