ab84092f097890012967e06705a69e31b0bba133
Davide Scarioni <davide.scarioni@gmail.com>
2023-05-18 22:51:26 +0200

    Update cards

--- blog/components/molecules/BlogCard/BlogCard.tsx
+++ blog/components/molecules/BlogCard/BlogCard.tsx
@@ -6,17 +6,25 @@ interface BlogCardProps {
 }
 
 const BlogCard = ({post}: BlogCardProps) => {
+  const category = post.categories[0].toLowerCase().replace(" ", "-");
   return (
-    <section>
+    <section className={`blog-card blog-card--${category}`}>
       <img
         src={urlFor(post?.mainImage)?.url()!}
-        alt=""
+        alt={post?.title}
+        className="blog-card__image"
       />
-      <div>
-        <h2>{post.title}</h2>
+      <div className="blog-card__category">{category}</div>
+      <div className="blog-card__body">
+        <div>
+          <h2 className="blog-card__title">{post?.title}</h2>
+        </div>
+        <div className="blog-card__description">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vulputate dolor metus, rhoncus congue sem cursus at. Aliquam erat volutpat. Aenean dictum facilisis nisl at ullamcorper.
+        </div>
       </div>
-      <div>
-        {post.description}
+      <div className="blog-card__author">
+        di {post?.author?.name}
       </div>
     </section>
   )
--- blog/pages/index.tsx
+++ blog/pages/index.tsx
@@ -22,14 +22,14 @@ export default function Home({ posts }: Props) {
 
       <Hero />
 
-      <div className="max-w-7xl mx-auto p-5">
+      <div className="max-w-7xl mx-auto p-5 blog-section">
         <h2 className="text-3xl mb-3">Blog</h2>
         {/* posts */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 py-3">
           {posts.map((post) => {
             return (
               <>
-                <Link href={`/posts/${post?.slug.current}`} key={post?._id}>
+                {/* <Link href={`/posts/${post?.slug.current}`} key={post?._id}>
                   <div className="group cursor-pointer overflow-hidden border rounded-lg">
                     {post.mainImage && (
                       <img
@@ -54,7 +54,7 @@ export default function Home({ posts }: Props) {
                       )}
                     </div>
                   </div>
-                </Link>
+                </Link> */}
                 <BlogCard post={post} key={post?._id} />
               </>
             );
@@ -69,6 +69,7 @@ export async function getStaticProps() {
   const query = `*[_type == 'post'] | order(publishedAt desc){
     _id,
     title,
+    "categories": categories[]->title,
     slug,
     author -> {
     name,
