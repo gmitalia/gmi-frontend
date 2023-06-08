9155e36723aef538081d5d8a83c2b3770d380885
Davide Scarioni <davide.scarioni@gmail.com>
2023-06-08 08:06:54 +0200

    Make Publish Date a required field

--- studio/schemas/post.js
+++ studio/schemas/post.js
@@ -47,6 +47,7 @@ export default {
       name: "publishedAt",
       title: "Published at",
       type: "datetime",
+      validation: (Rule) => Rule.required(),
     },
     {
       name: "body",
