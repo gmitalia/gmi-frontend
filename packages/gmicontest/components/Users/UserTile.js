c96407dc4a073f5f5f97c5a03db8c5845998da4c
MrSimsure <simonebenigni.dev@gmail.com>
2025-01-23 18:09:26 +0100

    add

--- packages/gmicontest/components/Users/UserTile.js
+++ packages/gmicontest/components/Users/UserTile.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import multiavatar from '@multiavatar/multiavatar/esm'
-import Link from "next/link";
 
 export const UserTile = (props) =>
 {
@@ -8,14 +7,15 @@ export const UserTile = (props) =>
 	const user = props.user;
 
 	return (
-		
-			<div
+	
+			<a
+			 href={`/user?user=${user.id}`}
 			 className="rounded-lg shadow-lg bg-white overflow-hidden relative flex flex-col justify-between cursor-pointer grid grid-cols-[150px_repeat(4,_1fr)] items-center gap-x-4"
 			 style={{width: "100%", height: "35px"}}
 			>
-				<div  style={{width: "80px"}} className="div-image">
+				<div  style={{height: "35px", aspectRatio: "1/1"}}>
 					<div
-					 style={{height: "100%", aspectRatio: "1/1"}}
+					 
 					 className="m-auto"
 					 dangerouslySetInnerHTML={{ __html: multiavatar("GM_USER_"+user.id) }}
 					/>
@@ -37,6 +37,6 @@ export const UserTile = (props) =>
 				<div style={{width: "20%"}}>
 					{user.judge_count}
 				</div>
-			</div>
+			</a>
 	);
 };
